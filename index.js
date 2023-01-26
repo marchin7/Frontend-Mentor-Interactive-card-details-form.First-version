2d4266a70ad54d42ed0b8567111779dff337efab
marchin7 <114770259+marchin7@users.noreply.github.com>
2023-01-26 19:44:02 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const thank = document.getElementById('thank');
 const confirmBtn = document.getElementById('confirm-btn');
 const continueBtn = document.getElementById('continue-btn');
 
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();  
+})
+
 confirmBtn.addEventListener('click', ()=>{
     form.style.display = 'none';
     thank.style.display = 'block';
